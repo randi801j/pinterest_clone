814f56cfef93d53dcd3fb0af9e0fa014d3b5f46e
Randy.Luc <randy.luc@RandyLucs-MBP.attlocal.net>
2023-07-03 20:06:29 -0700

    pinclone 2 day

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PinterestIcon from '@mui/icons-material/Pinterest';
-//import IconButton from '@mui/icons-material/IconButton'
 import styled from 'styled-components'
 
 function Header() {
@@ -10,6 +9,23 @@ function Header() {
         <LogoWrapper>
           <PinterestIcon/>
         </LogoWrapper>
+        <HomePageButton>
+          <a href='/'>HomePage</a>
+        </HomePageButton>
+        <FollowingButton>
+        <a href='/'>Following</a>
+        </FollowingButton>
+        <SearchWrapper>
+          <SearchBarWrapper>
+            {/* <IconButton>
+              <SearchIcon/>
+            </IconButton> */}
+            <form>
+              <input type='text'/>
+              <button type='submit'></button>
+            </form>
+          </SearchBarWrapper>
+        </SearchWrapper>
       </Wrapper>
     </div>
   )
@@ -31,4 +47,73 @@ const LogoWrapper = styled.div`
     font-size: 32px;
     cursor: pointer;
   }
+`
+//Dont repeat code, use inherate to repeat the same style for buttons. Other buttons will take in the Homebutton style.
+const HomeButton = styled.div` 
+  display: flex;
+  height: 48px;
+  min-width: 123px;
+  align-items:center;
+  justify-content: center;
+  border-radius: 24px;
+  cursor: pointer;
+`
+const HomePageButton = styled(HomeButton)`
+  background-color : rgb(17,17,17);
+
+  a{
+    text-decoration: none;
+    color:white;
+    font-weight:700;
+  }
+`
+const FollowingButton = styled(HomeButton)`
+  background-color : white;
+
+  a{
+    text-decoration: none;
+    color:black;
+    font-weight:700;
+  }
+  :hover{
+    display: flex;
+    height: 48px;
+    min-width: 123px;
+    align-items:center;
+    justify-content: center;
+    border-radius: 24px;
+    cursor: pointer;
+    background-color: #e1e1e1;
+  }
+`
+const SearchWrapper = styled.div`
+  flex:1;
+`
+const SearchBarWrapper = styled.div`
+  background-color: #efefef;
+  display: flex;
+  height:48px;
+  width:100%
+  border-radius: 50px;
+  border:none;
+  padding-left:10px;
+
+  form{
+    display:flex;
+    flex: 1;
+  }
+  form > input {
+    background-color:transparent;
+    border:none;
+    width:100%
+    margin-left:5px;
+    font-size:16px;
+  }
+  form > button{
+    display:none;
+  }
+  input:focus{
+    outline: none;
+  }
+  
 `
